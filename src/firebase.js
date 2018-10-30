f3822b658f747631672690bfab1b68f24a7b84e6
Cesar Ivan Romero <ivanrr308@gmail.com>
2018-10-29 21:19:28 -0500

    Update src/firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+
+  // Initialize Firebase
+  import firebase from 'firebase'
+  const config = {
+    apiKey: "AIzaSyDZLoc63uu2UvJdy-R8qjx4-RlDbHmy0AU",
+    authDomain: "infsys3816project-8503d.firebaseapp.com",
+    databaseURL: "https://infsys3816project-8503d.firebaseio.com",
+    projectId: "infsys3816project-8503d",
+    storageBucket: "infsys3816project-8503d.appspot.com",
+    messagingSenderId: "192625824557"
+  };
+  firebase.initializeApp(config);
+  export default firebase;
