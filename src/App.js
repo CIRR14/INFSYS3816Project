51b505108b52864fb45acf999d5e114b49a16205
Cesar Ivan Romero <ivanrr308@gmail.com>
2018-11-05 21:15:55 -0600

    Update src/App.js

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ class App extends Component {
     super();
     this.state = {
       currentResponsibility: '',
-      username: ''
+      username: '',
+      responsibilities: []
     }
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-  }
+    
+  } 
 
   handleChange(e) {
     this.setState({
@@ -23,7 +25,7 @@ class App extends Component {
     e.preventDefault(); //prevents default behavior(refreshing the page when 'submit' is clicked)
     const responsibilitiesRef = firebase.database().ref('responsibilities'); //stores responsibilities in Firebase database spot (responsibility)
     const responsibility = {
-      duty: this.state.currentResponsibility,
+      title: this.state.currentResponsibility,
       user: this.state.username
     } //grabs what the user typed in from the state and makes it into an object so it can be sent to Firebase
     responsibilitiesRef.push(responsibility); //sends a copy of the object so it can be stored in Firebase
@@ -31,7 +33,43 @@ class App extends Component {
       currentResponsibility: '',
       username: ''
     }); //clears out inputs so that another item can be added
+    
+    responsibilitiesRef.on('value', (snapshot) => {
+      console.log(snapshot.val());
+    });
   }
+  
+  ///
+  componentDidMount() {
+    const responsibilitiesRef = firebase.database().ref('responsibilities');
+    responsibilitiesRef.on('value', (snapshot) => {
+      let responsibilities = snapshot.val();
+      let newState = [];
+      for (let responsibility in responsibilities) {
+        newState.push({
+          duty: responsibilities,
+          title: responsibilities[responsibility].title,
+          user: responsibilities[responsibility].user
+        });
+      }
+      this.setState({
+        responsibilities: newState
+      });
+    });
+  }
+///
+
+removeResponsibility(responsibilityDuty) {
+  const responsibilityRef = firebase.database().ref(`/responsibilities/${responsibilityDuty}`);
+  responsibilityRef.remove();
+}
+
+// removeItem(itemId) {
+//   const itemRef = firebase.database().ref(`/items/${itemId}`);
+//   itemRef.remove();
+// }
+
+// <button onClick={() => this.removeItem(item.id)}>Remove Item</button>
 
   render() {
     return (
@@ -56,6 +94,15 @@ class App extends Component {
           <section className='display-item'>
             <div className='wrapper'>
               <ul>
+                {this.state.responsibilities.map((responsibility) => {
+                  return (
+                    <li key ={responsibility.duty}>
+                    <h3>{responsibility.title}</h3>
+                    <p> done by: {responsibility.user}</p>
+                    <button onClick={() => this.removeResponsibility(responsibility.id)}>Remove</button>
+                    </li>
+                  )
+                })}
               </ul>
             </div>
           </section>
