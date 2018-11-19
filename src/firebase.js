1057402c84972d634cf911b82d606de2b7f48b7c
Cesar Ivan Romero <ivanrr308@gmail.com>
2018-11-19 13:54:04 -0600

    Final

--- src/firebase.js
+++ src/firebase.js
@@ -10,4 +10,6 @@
     messagingSenderId: "192625824557"
   };
   firebase.initializeApp(config);
+  export const provider = new firebase.auth.GoogleAuthProvider();
+  export const auth = firebase.auth();
   export default firebase;
